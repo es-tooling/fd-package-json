ccd597e320ec967f26fa79efba632896f181f6df
hyperz111 <firzeroun11@gmail.com>
2025-11-16 00:07:23 +0700

    refactor

--- bench/bench.js
+++ bench/bench.js
@@ -3,15 +3,12 @@ import {up as empathic} from 'empathic/package';
 import {findPackagePath, findPackagePathSync} from 'fd-package-json';
 import {Bench} from 'tinybench';
 import {join} from 'node:path';
-
-function formatResult(task) {
-  return `${task.name} × ${Math.round(task.result.throughput.mean).toString()} (ops/s) \xb1${task.result.throughput.rme.toFixed(2)}% (${task.result.samples.length} runs sampled)`;
-}
+import {styleText} from 'node:util';
 
 const dirs = 'abcdefghij';
 const depths = [1, 3, 5, 10];
 
-depths.forEach(async (dep) => {
+for (const dep of depths) {
   const name = `${dep} depth${dep === 1 ? '' : 's'}`;
   const cwd = join(import.meta.dirname, 'fixture', ...dirs.substring(0, dep));
   const bench = new Bench({name, warmup: true});
@@ -26,6 +23,16 @@ depths.forEach(async (dep) => {
   await bench.run();
 
   console.log(`# ${name}`);
-  bench.tasks.forEach((task) => console.log(formatResult(task)));
+
+  const table = bench.table();
+  for (const row of table) {
+    for (const prop in row) {
+      const isName = prop === 'Task name';
+      console.log(
+        `${isName ? '-' : ' '} ${isName ? '' : `${prop}: `}${styleText(isName ? ['bold', 'underline'] : 'none', String(row[prop]))}`
+      );
+    }
+  }
+
   console.log('');
-});
+}
